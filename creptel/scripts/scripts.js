4a8cb2c3f42a9b85b82c23cbb2b9de92dff093b7
karunkmr <karun.kmr03@gmail.com>
2022-10-30 23:19:37 +0530

    musician page completed

--- creptel/scripts/scripts.js
+++ creptel/scripts/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-    alert("1")
+    $('.filter-box-content input').change((e) => {
+        if (e.target.checked) {
+            // console.log($(e.currentTarget).parent().find('.icon'))
+            $(e.currentTarget).parent().find('i').removeClass('fa-square').addClass('fa-square-check')
+        } else {
+            $(e.currentTarget).parent().find('i').removeClass('fa-square-check').addClass('fa-square')
+        }
+        
+    })
 })
